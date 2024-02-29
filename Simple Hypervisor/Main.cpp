242871fb5709f1adab01fbcc4401b8674dcccc20
Nana Bingies <nanabingies1@gmail.com>
2024-02-29 20:55:07 +0000

    Update Main.cpp

--- Simple Hypervisor/Main.cpp
+++ Simple Hypervisor/Main.cpp
@@ -3,6 +3,26 @@
 
 extern "C" {
 
+	auto DriverUnload(_In_ PDRIVER_OBJECT driver_object) -> void {
+		DbgPrint("[*] Terminating VMs on processors...\n");
+
+		if (driver_object->DeviceObject != nullptr) {
+			IoDeleteDevice(driver_object->DeviceObject);
+		}
+
+		DbgPrint("[*] Unloading Device Driver...\n");
+		return;
+	}
+
+	auto DefaultDispatch(_In_ PDEVICE_OBJECT, _In_ PIRP irp) -> NTSTATUS {
+		DbgPrint("[*] Default Dispatch\n");
+		irp->IoStatus.Status = STATUS_SUCCESS;
+		irp->IoStatus.Information = 0;
+
+		IoCompleteRequest(irp, IO_NO_INCREMENT);
+		return STATUS_SUCCESS;
+	}
+
 	auto DriverEntry(_In_ PDRIVER_OBJECT driver_object, _In_ PUNICODE_STRING registry_path) -> NTSTATUS {
 		DbgPrint("[*] Loading file %wZ\n", registry_path);
 
@@ -21,6 +41,11 @@ extern "C" {
 		if (!NT_SUCCESS(status))	return STATUS_FAILED_DRIVER_ENTRY;
 
 		DbgPrint("[*] Successfully created device object\n");
+
+		for (ulong idx = 0; idx < IRP_MJ_MAXIMUM_FUNCTION; ++idx) {
+			driver_object->MajorFunction[idx] = DefaultDispatch;
+		}
+		driver_object->DriverUnload = DriverUnload;
 
 		DbgPrint("[*] The hypervisor has been installed.\n");
 
