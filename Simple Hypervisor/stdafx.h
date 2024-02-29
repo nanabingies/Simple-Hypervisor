74ea8ceeea37a3deae0807e3064195dee2082a42
Nana Bingies <nanabingies1@gmail.com>
2024-02-29 20:43:19 +0000

    Update stdafx.h

--- Simple Hypervisor/stdafx.h
+++ Simple Hypervisor/stdafx.h
@@ -4,13 +4,7 @@
 #include <ntddk.h>
 #include <intrin.h>
 #include <windef.h>
-
-#include "Ept.hpp"
-#include "Vmx.hpp"
-#include "ia32.hpp"
-#include "Vmcs.hpp"
-#include "VmUtils.hpp"
-
+#include <ntdef.h>
 
 using uint8_t = unsigned char;
 using uint16_t = unsigned short;
@@ -18,6 +12,7 @@ using uint32_t = unsigned int;
 using uint64_t = unsigned long long;
 using ulong_ptr = unsigned long long;
 
+using int8_t = signed char;
 using int32_t = signed int;
 using int64_t = signed long long;
 
