dee9ea143fc470d31d53fe6be7a1b04e28977983
Nana Bingies <nanabingies1@gmail.com>
2024-02-29 21:48:52 +0000

    Update Vmx.cpp

--- Simple Hypervisor/Vmx.cpp
+++ Simple Hypervisor/Vmx.cpp
@@ -3,5 +3,36 @@
 namespace vmx {
 	auto VmxIsVmxAvailable() -> bool {
 		PAGED_CODE();
+
+		//
+		// Check VMX Support for all Logical Processors
+		//
+		if (!VmxIsVmxSupport())	return false;
+
+		//
+		// Check Bios Lock Bit
+		//
+		if (!VmxCheckBiosLock())	return FALSE;
+
+		//
+		// Enable VMXE for all processors
+		//
+		VmxEnableCR4();
+
+		//
+		// Check for EPT support for all processors
+		//
+		//if (CheckEPTSupport() == FALSE)	return FALSE;
+
+
+		DbgPrint("[*] Initial checks completed.\n");
+
+		//
+		// Build MTRR Map
+		//
+		//if (EptBuildMTRRMap() == FALSE)	return FALSE;
+		LOG("[*] MTRR built successfully\n");
+
+		return TRUE;
 	}
 }
