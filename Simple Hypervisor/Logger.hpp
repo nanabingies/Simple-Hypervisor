76581803d9e475ca9cb055a6ff1caebb138b6b1f
Nana Bingies <nanabingies1@gmail.com>
2024-02-29 22:13:29 +0000

    Update Logger.hpp

--- Simple Hypervisor/Logger.hpp
+++ Simple Hypervisor/Logger.hpp
@@ -4,9 +4,18 @@
 #define LOG(format, ...) \
 	logger::log(format, __VA_ARGS__)
 
+#define LOG_EEROR(format, ...) \
+	logger::error(format, __VA_ARGS__)
+
 namespace logger {
 	template <typename ... T>
 	__forceinline void log(const char* format, T const& ... args)
+	{
+		DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, format, args ...);
+	}
+
+	template <typename ... T>
+	__forceinline void error(const char* format, T const& ... args)
 	{
 		DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, format, args ...);
 	}
