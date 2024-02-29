4e48a5cfff9efbf743c489c26eb5e48d84384bc8
Nana Bingies <nanabingies1@gmail.com>
2024-02-29 22:21:14 +0000

    Update Logger.hpp

--- Simple Hypervisor/Logger.hpp
+++ Simple Hypervisor/Logger.hpp
@@ -4,19 +4,20 @@
 #define LOG(format, ...) \
 	logger::log(format, __VA_ARGS__)
 
-#define LOG_ERROR(format, ...) \
-	LOG("[!] failed at %s:%d, (0x%lX)\n", __FILE__, __LINE__, GetLastError())
+#define LOG_ERROR() \
+	logger::error()
 
 namespace logger {
 	template <typename ... T>
 	__forceinline void log(const char* format, T const& ... args)
 	{
-		DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, format, args ...);
+		DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, format, args ...);
 	}
 
 	template <typename ... T>
-	__forceinline void error(const char* format, T const& ... args)
+	__forceinline void error()
 	{
-		DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, format, args ...);
+		DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, 
+			"[!] failed at %s:%d, (0x%lX)\n", __FILE__, __LINE__, GetLastError());
 	}
 }
