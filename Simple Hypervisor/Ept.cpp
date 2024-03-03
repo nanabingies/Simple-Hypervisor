72256184c533b26396d0447c53f0862b5fbe2eeb
Nana Bingies <nanabingies1@gmail.com>
2024-03-03 01:35:41 +0000

    Update Ept.cpp

---
+++ Simple Hypervisor/Ept.cpp
@@ -0,0 +1,36 @@
+#include "ept.hpp"
+
+namespace ept {
+	auto checkEPTSupport() -> bool {
+		PAGED_CODE();
+
+		PROCESSOR_NUMBER processor_number;
+		GROUP_AFFINITY affinity, old_affinity;
+
+		for (unsigned iter = 0; iter < g_num_processors; iter++) {
+			KeGetProcessorNumberFromIndex(iter, &processor_number);
+
+			RtlSecureZeroMemory(&affinity, sizeof(GROUP_AFFINITY));
+			affinity.Group = processor_number.Group;
+			affinity.Mask = static_cast<KAFFINITY>(1) << processor_number.Number;
+			KeSetSystemGroupAffinityThread(&affinity, &old_affinity);
+
+			auto irql = KeRaiseIrqlToDpcLevel();
+
+			ia32_vmx_ept_vpid_cap_register ept_cap;
+			ept_cap.flags = __readmsr(IA32_VMX_EPT_VPID_CAP);
+
+			if (!ept_cap.page_walk_length_4 || !ept_cap.memory_type_write_back || !ept_cap.invept ||
+				!ept_cap.invept_single_context || !ept_cap.invept_all_contexts || !ept_cap.invvpid ||
+				!ept_cap.invvpid_individual_address || !ept_cap.invvpid_all_contexts ||
+				!ept_cap.invvpid_single_context || !ept_cap.invvpid_single_context_retain_globals) {
+				return false;
+			}
+
+			KeLowerIrql(irql);
+			KeRevertToUserGroupAffinityThread(&old_affinity);
+		}
+
+		return true;
+	}
+}
