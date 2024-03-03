2c1cac2655d190a23158f03eaf586ac74a280685
Nana Bingies <nanabingies1@gmail.com>
2024-03-03 01:57:28 +0000

    Update VmUtils.hpp

--- Simple Hypervisor/VmUtils.hpp
+++ Simple Hypervisor/VmUtils.hpp
@@ -4,5 +4,42 @@
 #define DOS_NAME	L"\\DosDevices\\Hypervisor"
 #define VMM_POOL	'tesT'
 
+struct _vmm_context {
+	uint64_t	vmxon_region_virt_addr;				// Virtual address of VMXON Region
+	uint64_t	vmxon_region_phys_addr;				// Physical address of VMXON Region
+
+	uint64_t	vmcs_region_virt_addr;				// virtual address of VMCS Region
+	uint64_t	vmcs_region_phys_addr;				// Physical address of VMCS Region
+
+	uint64_t	io_bitmap_a_virt_addr;              // Virtual address of I/O bitmap A
+	uint64_t	io_bitmap_a_phys_addr;              // Physical address
+
+	uint64_t	io_bitmap_b_virt_addr;				// Virtual address of I/O bitmap B
+	uint64_t	io_bitmap_b_phys_addr;				// Plysical address
+
+	uint64_t	msr_bitmap_virt_addr;				// Virtual address of MSR bitmap
+	uint64_t	msr_bitmap_phys_addr;				// Physical address
+
+	uint64_t	host_stack;							// Stack of the VM Exit Handler
+
+	uint64_t	guest_memory;						// Guest RSP
+
+	uint64_t	guest_rsp;
+	uint64_t	guest_rip;
+
+	uint64_t	host_rsp;
+	uint64_t	host_rip;
+
+	struct {
+		uint64_t	g_stack_pointer_for_returning;
+		uint64_t	g_base_pointer_for_returning;
+	};
+
+	uint64_t	ept_ptr;
+	uint64_t	ept_pml4;
+	//ept_state* ept_state;
+};
+
 extern bool VmOff;
 extern unsigned g_num_processors;
+extern _vmm_context* vmm_context;
