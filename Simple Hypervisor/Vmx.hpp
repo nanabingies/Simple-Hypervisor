45d6d2fcfbc69c86a1938e227d01993376a5ee83
Nana Bingies <nanabingies1@gmail.com>
2024-03-03 01:24:43 +0000

    Update Vmx.hpp

--- Simple Hypervisor/Vmx.hpp
+++ Simple Hypervisor/Vmx.hpp
@@ -3,14 +3,14 @@
 //#include "vmutils.hpp"
 
 namespace vmx {
-	auto VmxIsVmxAvailable() -> bool;
-	auto VmxIsVmxSupport() -> bool;
-	auto VmxCheckBiosLock() -> bool;
-	auto VmxEnableCR4() -> void;
+	auto vmxIsVmxAvailable() -> bool;
+	auto vmxIsVmxSupport() -> bool;
+	auto vmxCheckBiosLock() -> bool;
+	auto vmxEnableCR4() -> void;
 }
 
 namespace hv {
-	auto VirtualizeAllProcessors() -> bool;
+	auto virtualizeAllProcessors() -> bool;
 
-	auto DevirtualizeAllProcessors() -> void;
+	auto devirtualizeAllProcessors() -> void;
 }
