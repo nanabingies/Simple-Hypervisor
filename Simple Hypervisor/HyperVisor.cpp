b817653f33378ad1694b989b405242e9819c6ea8
Nana Bingies <nanabingies1@gmail.com>
2024-03-03 02:02:48 +0000

    Update HyperVisor.cpp

--- Simple Hypervisor/HyperVisor.cpp
+++ Simple Hypervisor/HyperVisor.cpp
@@ -17,10 +17,53 @@ namespace hv {
 		// ExAllocatePool2 - Memory is zero initialized unless POOL_FLAG_UNINITIALIZED is specified.
 		//
 		vmm_context = reinterpret_cast<_vmm_context*>
-			(ExAllocatePoolWithTag(NonPagedPool, sizeof(_vmm_context) * g_num_processors, VMM_POOL_TAG);
+			(ExAllocatePoolWithTag(NonPagedPool, sizeof(_vmm_context) * g_num_processors, VMM_POOL_TAG));
+		if (!vmm_context) {
+			LOG("[-] Failed to allocate memory for vmm_context\n");
+			LOG_ERROR();
+			return false;
+		}
 
 		for (unsigned iter = 0; iter < g_num_processors; iter++) {
+			KeGetProcessorNumberFromIndex(iter, &processor_number);
 
+			RtlSecureZeroMemory(&affinity, sizeof(GROUP_AFFINITY));
+			affinity.Group = processor_number.Group;
+			affinity.Mask = (KAFFINITY)1 << processor_number.Number;
+			KeSetSystemGroupAffinityThread(&affinity, &old_affinity);
+			auto irql = KeRaiseIrqlToDpcLevel();
+
+			//
+			// Allocate Memory for VMXON & VMCS regions and initialize
+			//
+			if (!vmxAllocateVmxonRegion(processor_number.Number))		return false;
+			if (!vmxAllocateVmcsRegion(processor_number.Number))		return false;
+
+			//
+			// Allocate space for VM EXIT Handler
+			//
+			if (!vmxAllocateVmExitStack(processor_number.Number))		return false;
+
+			//
+			// Allocate memory for IO Bitmap
+			//
+			if (!vmxAllocateIoBitmapStack(processor_number.Number))			return false;
+
+			//
+			// Future: Add MSR Bitmap support
+			// Fix: Added MSR Bitmap support
+			//
+			if (!vmxAllocateMsrBitmap(processor_number.Number))			return false;
+
+			//
+			// Setup EPT support for that processor
+			//
+			if (!initializeEpt(processor_number.Number))				return false;
+
+			KeLowerIrql(irql);
+			KeRevertToUserGroupAffinityThread(&old_affinity);
 		}
+
+		return true;
 	}
 }
