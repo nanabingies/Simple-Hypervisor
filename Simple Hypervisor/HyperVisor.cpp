0568c14498a9f6b5cf5f45edc1cd9ba55da93e86
Nana Bingies <nanabingies1@gmail.com>
2024-03-03 01:49:10 +0000

    Update HyperVisor.cpp

--- Simple Hypervisor/HyperVisor.cpp
+++ Simple Hypervisor/HyperVisor.cpp
@@ -2,6 +2,23 @@
 
 namespace hv {
 	auto virtualizeAllProcessors() -> bool {
+		PAGED_CODE();
 
+		//
+		// This was more of an educational project so only one Logical Processor was chosen and virtualized
+		// TODO : Add support for multiple processors
+		// Update: Support for multiple processors added
+		//
+
+		PROCESSOR_NUMBER processor_number;
+		GROUP_AFFINITY affinity, old_affinity;
+
+		//
+		// ExAllocatePool2 - Memory is zero initialized unless POOL_FLAG_UNINITIALIZED is specified.
+		//
+
+		for (unsigned iter = 0; iter < g_num_processors; iter++) {
+
+		}
 	}
 }
