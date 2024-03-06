7795022694a793194bbafb435034b00c75e3b9c4
Nana Bingies <nanabingies1@gmail.com>
2024-03-06 13:49:51 +0000

    Update Vmx.hpp

--- Simple Hypervisor/Vmx.hpp
+++ Simple Hypervisor/Vmx.hpp
@@ -2,22 +2,22 @@
 using uchar = unsigned char;
 
 namespace vmx {
-	auto vmxIsVmxAvailable() -> bool;
-	auto vmxIsVmxSupport() -> bool;
-	auto vmxCheckBiosLock() -> bool;
-	auto vmxEnableCR4() -> void;
+	auto vmx_is_vmx_available() -> bool;
+	auto vmx_is_vmx_support() -> bool;
+	auto vmx_check_bios_lock() -> bool;
+	auto vmx_enable_cr4() -> void;
 
-	auto vmxAllocateVmxonRegion(uchar) -> bool;
-	auto vmxAllocateVmcsRegion(uchar) -> bool;
-	auto vmxAllocateVmExitStack(uchar) -> bool;
-	auto vmxAllocateIoBitmapStack(uchar) -> bool;
-	auto vmxAllocateMsrBitmap(uchar) -> bool;
+	auto vmx_allocate_vmxon_region(uchar) -> bool;
+	auto vmx_allocate_vmcs_region(uchar) -> bool;
+	auto vmx_allocate_vmexit_stack(uchar) -> bool;
+	auto vmx_allocate_io_bitmap_stack(uchar) -> bool;
+	auto vmx_allocate_msr_bitmap(uchar) -> bool;
 }
 
 namespace hv {
-	auto virtualizeAllProcessors() -> bool;
+	auto virtualize_all_processors() -> bool;
 
-	auto devirtualizeAllProcessors() -> void;
+	auto devirtualize_all_processors() -> void;
 
-	auto launchVM(unsigned __int64) -> unsigned __int64;
+	auto launch_vm(unsigned __int64) -> unsigned __int64;
 }
