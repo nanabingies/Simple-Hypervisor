8b7bd3ce7dc2aa86f71e8635a87eb44df4ca72f9
Nana Bingies <nanabingies1@gmail.com>
2024-03-07 16:18:57 +0000

    Update Vmcs.hpp

--- Simple Hypervisor/Vmcs.hpp
+++ Simple Hypervisor/Vmcs.hpp
@@ -27,6 +27,79 @@ enum SEGREGS {
     TR
 };
 
-extern "C" auto inline asm_setup_vmcs(unsigned long) -> EVmErrors;
+typedef struct _vmx_gdtentry64
+{
+    uint64_t Base;
+    uint32_t Limit;
+    union
+    {
+        struct
+        {
+            uint8_t Flags1;
+            uint8_t Flags2;
+            uint8_t Flags3;
+            uint8_t Flags4;
+        } Bytes;
+        struct
+        {
+            uint16_t SegmentType : 4;
+            uint16_t DescriptorType : 1;
+            uint16_t Dpl : 2;
+            uint16_t Present : 1;
+
+            uint16_t Reserved : 4;
+            uint16_t System : 1;
+            uint16_t LongMode : 1;
+            uint16_t DefaultBig : 1;
+            uint16_t Granularity : 1;
+
+            uint16_t Unusable : 1;
+            uint16_t Reserved2 : 15;
+        } Bits;
+        uint32_t AccessRights;
+    };
+    uint16_t Selector;
+} vmx_gdtentry64, *pvmx_gdtentry64;
+
+typedef union _kgdtentry64
+{
+    struct
+    {
+        uint16_t LimitLow;
+        uint16_t BaseLow;
+        union
+        {
+            struct
+            {
+                uint8_t BaseMiddle;
+                uint8_t Flags1;
+                uint8_t Flags2;
+                uint8_t BaseHigh;
+            } Bytes;
+            struct
+            {
+                uint32_t BaseMiddle : 8;
+                uint32_t Type : 5;
+                uint32_t Dpl : 2;
+                uint32_t Present : 1;
+                uint32_t LimitHigh : 4;
+                uint32_t System : 1;
+                uint32_t LongMode : 1;
+                uint32_t DefaultBig : 1;
+                uint32_t Granularity : 1;
+                uint32_t BaseHigh : 8;
+            } Bits;
+        };
+        uint32_t BaseUpper;
+        uint32_t MustBeZero;
+    };
+    struct
+    {
+        int64_t DataLow;
+        int64_t DataHigh;
+    };
+} kgdtentry64, * pkgdtentry64;
+
+extern "C" auto inline asm_setup_vmcs(unsigned long)->EVmErrors;
 
 auto setup_vmcs(unsigned long, void*) -> EVmErrors;
