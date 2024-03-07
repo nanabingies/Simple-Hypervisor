e44e8831e373f613c44ec091d23943ae869a28de
Nana Bingies <nanabingies1@gmail.com>
2024-03-07 21:06:31 +0000

    Update VmExit.cpp

--- Simple Hypervisor/VmExit.cpp
+++ Simple Hypervisor/VmExit.cpp
@@ -1,5 +1,8 @@
 #include "stdafx.h"
 
 namespace vmexit {
-
+	auto vmexit_handler(void* _guest_registers) -> void {
+		auto guest_regs = reinterpret_cast<guest_registers*>(_guest_registers);
+		if (!guest_regs)	return;
+	}
 }
