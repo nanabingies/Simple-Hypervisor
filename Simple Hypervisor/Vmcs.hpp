86e7543ceb8c88193ac1490f36d15ee43eb82cb7
Nana Bingies <nanabingies1@gmail.com>
2024-03-07 16:56:20 +0000

    Update Vmcs.hpp

--- Simple Hypervisor/Vmcs.hpp
+++ Simple Hypervisor/Vmcs.hpp
@@ -102,13 +102,18 @@ typedef union _kgdtentry64
 
 extern "C" {
     auto inline asm_setup_vmcs(unsigned long) -> EVmErrors;
+
     auto inline __stdcall asm_host_continue_execution() -> unsigned __int64;
     auto inline asm_guest_continue_execution() -> void;
+
     auto inline asm_get_ldtr() -> unsigned short;
     auto inline asm_get_tr() -> unsigned short;
     auto inline asm_get_idt_limit() -> unsigned short;
     auto inline asm_get_gdt_limit() -> unsigned short;
     auto inline asm_get_rflags() -> unsigned __int64;
+
+    auto inline asm_get_gdt_base() -> unsigned __int64;
+    auto inline asm_get_idt_base() -> unsigned __int64;
 }
 
 auto setup_vmcs(unsigned long, void*) -> EVmErrors;
