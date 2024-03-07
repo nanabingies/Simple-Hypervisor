60c4fcf6c38359f4c5f37f036e1cea345da1867b
Nana Bingies <nanabingies1@gmail.com>
2024-03-07 16:18:48 +0000

    Update VmUtils.hpp

--- Simple Hypervisor/VmUtils.hpp
+++ Simple Hypervisor/VmUtils.hpp
@@ -7,6 +7,26 @@
 
 #define HOST_STACK_SIZE  (20 * PAGE_SIZE)
 
+using guest_registers = struct guest_registers {
+	uint64_t	RAX;
+	uint64_t	RBX;
+	uint64_t	RCX;
+	uint64_t	RDX;
+	uint64_t	RSP;
+	uint64_t	RBP;
+	uint64_t	RSI;
+	uint64_t	RDI;
+	uint64_t	R8;
+	uint64_t	R9;
+	uint64_t	R10;
+	uint64_t	R11;
+	uint64_t	R12;
+	uint64_t	R13;
+	uint64_t	R14;
+	uint64_t	R15;
+	__m128	xmm[6];
+} ;
+
 struct _vmm_context {
 	uint64_t	vmxon_region_virt_addr;				// Virtual address of VMXON Region
 	uint64_t	vmxon_region_phys_addr;				// Physical address of VMXON Region
