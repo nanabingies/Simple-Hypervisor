f4ec006f6fc05788a2e4555a1975159f4adfda60
Nana Bingies <nanabingies1@gmail.com>
2024-03-12 02:28:09 +0000

    Update VmUtils.hpp

--- Simple Hypervisor/VmUtils.hpp
+++ Simple Hypervisor/VmUtils.hpp
@@ -94,3 +94,18 @@ inline uint64_t virtual_to_physical_address(void* virtual_address) {
 	physAddr = MmGetPhysicalAddress(virtual_address);
 	return physAddr.QuadPart;
 }
+
+extern "C" {
+	NTKERNELAPI
+		_IRQL_requires_max_(APC_LEVEL)
+		_IRQL_requires_min_(PASSIVE_LEVEL) _IRQL_requires_same_ VOID
+		KeGenericCallDpc(_In_ PKDEFERRED_ROUTINE Routine, _In_opt_ PVOID Context);
+
+	NTKERNELAPI
+		_IRQL_requires_(DISPATCH_LEVEL) _IRQL_requires_same_ VOID
+		KeSignalCallDpcDone(_In_ PVOID SystemArgument1);
+
+	NTKERNELAPI
+		_IRQL_requires_(DISPATCH_LEVEL) _IRQL_requires_same_ LOGICAL
+		KeSignalCallDpcSynchronize(_In_ PVOID SystemArgument2);
+}
