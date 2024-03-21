04847764dd11c30ad11e5f2972a86551742c5f26
Nana Bingies <nanabingies1@gmail.com>
2024-03-21 07:22:49 +0000

    Update Ept.hpp

--- Simple Hypervisor/Ept.hpp
+++ Simple Hypervisor/Ept.hpp
@@ -39,6 +39,34 @@ enum inv_ept_type {
 	SingleContextInvalidationExceptGlobal = 3,
 };
 
+using mtrr_entry = struct _mtrr_entry {
+	bool		mtrr_enabled;
+	bool		mtrr_fixed;
+	uint64_t	memory_type;
+	uint64_t	physical_address_start;
+	uint64_t	physical_address_end;
+};
+
+using ept_split_page = struct _ept_split_page {
+	DECLSPEC_ALIGN(PAGE_SIZE)	ept_pte ept_pte[512];
+	ept_pde_2mb*				ept_pde;
+	LIST_ENTRY					split_pages;
+};
+
+using ept_page_table = struct _ept_page_table {
+	DECLSPEC_ALIGN(PAGE_SIZE)	ept_pml4e ept_pml4[EPTPML4ENTRIES];
+	DECLSPEC_ALIGN(PAGE_SIZE)	ept_pdpte ept_pdpte[EPTPDPTEENTRIES];
+	DECLSPEC_ALIGN(PAGE_SIZE)	ept_pde_2mb ept_pde[EPTPML4ENTRIES][EPTPDPTEENTRIES];
+	ept_entry**					dynamic_pages;
+	uint64_t					dynamic_pages_count;
+};
+
+using ept_state = struct _ept_state {
+	uint64_t		guest_address_width_value;
+	ept_pointer*	ept_ptr;
+	ept_page_table* ept_page_table;
+};
+
 namespace ept {
 	auto check_ept_support() -> bool;
 }
