13d0921a608d2aba2a65633c9884ac3e514680da
Nana Bingies <nanabingies1@gmail.com>
2024-03-22 05:20:52 +0000

    Update Ept.hpp

--- Simple Hypervisor/Ept.hpp
+++ Simple Hypervisor/Ept.hpp
@@ -43,9 +43,9 @@ enum inv_ept_type {
 using mtrr_entry = struct _mtrr_entry {
 	bool		mtrr_enabled;
 	bool		mtrr_fixed;
-	uint64_t	memory_type;
-	uint64_t	physical_address_start;
-	uint64_t	physical_address_end;
+	unsigned long long	memory_type;
+	unsigned long long	physical_address_start;
+	unsigned long long	physical_address_end;
 };
 
 using ept_split_page = struct _ept_split_page {
@@ -59,15 +59,54 @@ using ept_page_table = struct _ept_page_table {
 	DECLSPEC_ALIGN(PAGE_SIZE)	ept_pdpte ept_pdpte[EPTPDPTEENTRIES];
 	DECLSPEC_ALIGN(PAGE_SIZE)	ept_pde_2mb ept_pde[EPTPML4ENTRIES][EPTPDPTEENTRIES];
 	ept_entry** dynamic_pages;
-	uint64_t					dynamic_pages_count;
+	unsigned long long					dynamic_pages_count;
 };
 
 using ept_state = struct _ept_state {
-	uint64_t		guest_address_width_value;
+	unsigned long long		guest_address_width_value;
 	ept_pointer* ept_ptr;
 	ept_page_table* ept_page_table;
 };
 
+using vmx_non_root_memory = struct vmx_non_root_memory {
+	void* pre_allocated_buffer;
+};
+
 namespace ept {
+	static inline unsigned long long g_mtrr_num = 0;
+	static inline const unsigned long max_ept_walk_length = 0x4;
+
+	inline unsigned long long g_default_memory_type;
+
 	auto check_ept_support() -> bool;
+
+	auto initialize_ept(unsigned char) -> bool;
+
+	auto ept_build_mtrr_map() -> bool;
+
+	auto create_ept_state(ept_state*) -> bool;
+
+	auto handle_ept_violation(uint64_t, uint64_t) -> void;
+
+	auto setup_pml2_entries(ept_state*, ept_pde_2mb*, uint64_t) -> void;
+
+	auto is_in_range(uint64_t, uint64_t, uint64_t) -> bool;
+
+	auto is_valid_for_large_page(uint64_t) -> bool;
+
+	auto get_pte_entry(ept_page_table*, uint64_t) -> ept_pte*;
+
+	auto get_pde_entry(ept_page_table*, uint64_t) -> ept_pde_2mb;
+
+	auto ept_construct_tables(ept_entry*, uint64_t, uint64_t, ept_page_table*) -> ept_entry*;
+
+	auto ept_init_table_entry(ept_entry*, uint64_t, uint64_t) -> void;
+
+	auto ept_allocate_ept_entry(ept_page_table*) -> ept_entry*;
+
+	auto ept_get_memory_type(uint64_t, bool) -> uint64_t;
+
+	auto ept_inv_global_entry() -> uint64_t;
+
+	auto split_pde(ept_page_table*, void*, uint64_t) -> void;
 }
