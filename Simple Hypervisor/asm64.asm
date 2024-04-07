6f10e7268acd2b187d9bfd056343dfbf86ffa459
Nana Bingies <nanabingies1@gmail.com>
2024-04-07 23:58:37 +0000

    Update asm64.asm

--- Simple Hypervisor/asm64.asm
+++ Simple Hypervisor/asm64.asm
@@ -64,78 +64,82 @@ endm
 asm_host_continue_execution proc
 	;int 3		; A VM Exit just occured
 
-	pushfq
-	SAVE_GP
+	push rax
+	push rbx
+	push rcx
+	push rdx
+	push rsi
+	push rdi
+	push rbp
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
 
-	sub     rsp, 060h
-	movdqa  xmmword ptr [rsp], xmm0
-    movdqa  xmmword ptr [rsp + 10h], xmm1
-    movdqa  xmmword ptr [rsp + 20h], xmm2
-    movdqa  xmmword ptr [rsp + 30h], xmm3
-    movdqa  xmmword ptr [rsp + 40h], xmm4
-    movdqa  xmmword ptr [rsp + 50h], xmm5
+	sub rsp, 0108h ; 16 xmm registers... and +8 bytes for alignment...
+	movaps [rsp], xmm0
+	movaps [rsp + 010h], xmm1
+	movaps [rsp + 020h], xmm2
+	movaps [rsp + 030h], xmm3
+	movaps [rsp + 040h], xmm4
+	movaps [rsp + 050h], xmm5
+	movaps [rsp + 060h], xmm6
+	movaps [rsp + 070h], xmm7
+	movaps [rsp + 080h], xmm8
+	movaps [rsp + 090h], xmm9
+	movaps [rsp + 0A0h], xmm10
+	movaps [rsp + 0B0h], xmm11
+	movaps [rsp + 0C0h], xmm12
+	movaps [rsp + 0D0h], xmm13
+	movaps [rsp + 0E0h], xmm14
+	movaps [rsp + 0F0h], xmm15
 
 	mov rcx, rsp
 	sub rsp, 020h
 	call ?vmexit_handler@vmexit@@YAFPEAX@Z			; handle VM exit 
 	add rsp, 020h
 
-	movdqa  xmm0, xmmword ptr [rsp]
-    movdqa  xmm1, xmmword ptr [rsp + 10h]
-    movdqa  xmm2, xmmword ptr [rsp + 20h]
-    movdqa  xmm3, xmmword ptr [rsp + 30h]
-    movdqa  xmm4, xmmword ptr [rsp + 40h]
-    movdqa  xmm5, xmmword ptr [rsp + 50h]
-	add     rsp,  060h
+	movups xmm0, [rsp]
+	movups xmm1, [rsp + 010h]
+	movups xmm2, [rsp + 020h]
+	movups xmm3, [rsp + 030h]
+	movups xmm4, [rsp + 040h]
+	movups xmm5, [rsp + 050h]
+	movups xmm6, [rsp + 060h]
+	movups xmm7, [rsp + 070h]
+	movups xmm8, [rsp + 080h]
+	movups xmm9, [rsp + 090h]
+	movups xmm10, [rsp + 0A0h]
+	movups xmm11, [rsp + 0B0h]
+	movups xmm12, [rsp + 0C0h]
+	movups xmm13, [rsp + 0D0h]
+	movups xmm14, [rsp + 0E0h]
+	movups xmm15, [rsp + 0F0h]
+	add rsp, 0108h ; 16 xmm registers... and +8 bytes for alignment...
 
-	cmp     al, 0
-    jnz      exit
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rbp 
+	pop rdi
+	pop rsi
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
 
-	RESTORE_GP
-
-    ;mov     edi, 681Eh
-    ;vmread  rdx, rdi
-    ;mov     ecx, 440Ch
-    ;vmread  rcx, rcx
-    ;add     rdx, rcx
-    ;vmwrite rdi, rdx
     vmresume
 
-exit:
-	sub rsp, 20h
-    ;call ?return_rsp_for_vmxoff@@YA_KXZ
-    add rsp, 20h
-
-	push rax
-
-    sub rsp, 20h
-    ;call ?return_rip_for_vmxoff@@YA_KXZ
-    add rsp, 20h
-
-	push rax
-
-    mov rcx,rsp
-    mov rsp,[rcx+8h]
-    mov rax,[rcx]
-    push rax
-
-    mov r15, [rcx + 10h]
-    mov r14, [rcx + 18h]
-    mov r13, [rcx + 20h]
-    mov r12, [rcx + 28h]
-    mov r11, [rcx + 30h]
-    mov r10, [rcx + 38h]
-    mov r9,  [rcx + 40h]
-    mov r8,  [rcx + 48h]
-    mov rdi, [rcx + 50h]
-    mov rsi, [rcx + 58h]
-    mov rbp, [rcx + 60h]
-    mov rbx, [rcx + 70h]
-    mov rdx, [rcx + 78h]
-    mov rax, [rcx + 88h]
-    mov rcx, [rcx + 80h]
-
-	ret
+	int 3	; vmresume failed
 asm_host_continue_execution ENDP
 
 ;----------------------------------------------------------------------------------------------------
@@ -176,7 +180,26 @@ asm_setup_vmcs endp
 ;----------------------------------------------------------------------------------------------------
 
 asm_hv_launch proc
+    pushfq
 
+    mov rcx, 0681Ch		; VMCS_GUEST_RSP
+	vmwrite rcx, rsp	; current rsp pointer...
+
+	mov rcx, 0681Eh		; VMCS_GUEST_RIP
+	lea rdx, done		;
+	vmwrite rcx, rdx	; return 00h on success...
+	vmlaunch
+
+    pushfq				; push rflags to the stack then put it into rax...
+	pop rax				;
+
+	popfq				; restore rflags back to what it was in the c++ code...
+	ret
+
+done:
+	popfq				; restore flags and return back to c++ code...
+	mov rax, 00h
+	ret
 asm_hv_launch endp
 
 ;----------------------------------------------------------------------------------------------------
