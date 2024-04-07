87ed0f87c2175235be0b74bcbe6336e1ed8a68f6
Nana Bingies <nanabingies1@gmail.com>
2024-04-07 20:38:44 +0000

    Update Main.cpp

--- Simple Hypervisor/Main.cpp
+++ Simple Hypervisor/Main.cpp
@@ -12,10 +12,7 @@ extern "C" {
 		//LOG("[*] Terminating VMs on processors...");
 
 		// Uninstall vmx on all processors
-		if (!vm_off) {
-			devirtualize_all_processors();
-			vm_off = true;
-		}
+		devirtualize_all_processors();
 
 		//if (driver_object->DeviceObject != nullptr) {
 		//	IoDeleteDevice(driver_object->DeviceObject);
@@ -35,7 +32,6 @@ extern "C" {
 		return STATUS_SUCCESS;
 	}
 
-	bool vm_off;
 	unsigned g_num_processors;
 
 	auto DriverEntry(_In_ PDRIVER_OBJECT, _In_ PUNICODE_STRING) -> NTSTATUS {
@@ -43,8 +39,6 @@ extern "C" {
 		using hv::launch_vm;
 		using hv::launch_all_vmms;
 		using vmx::vmx_is_vmx_available;
-
-		vm_off = false;
 
 		// Opt-in to using non-executable pool memory on Windows 8 and later.
 		// https://msdn.microsoft.com/en-us/library/windows/hardware/hh920402(v=vs.85).aspx
