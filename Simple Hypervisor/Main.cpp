a410cd9564323e6f7856572a04c3ecaa43c9191d
Nana Bingies <nanabingies1@gmail.com>
2024-05-02 09:54:35 +0000

    Update Main.cpp

--- Simple Hypervisor/Main.cpp
+++ Simple Hypervisor/Main.cpp
@@ -4,14 +4,21 @@
 
 extern "C" {
 
-	auto DriverUnload(_In_ PDRIVER_OBJECT) -> void {
+	auto DriverUnload(_In_ PDRIVER_OBJECT driver_object) -> void {
 		using hv::devirtualize_all_processors;
 
+		UNICODE_STRING us_string{};
+		RtlInitUnicodeString(PUNICODE_STRING(&us_string), DOS_NAME);
+		IoDeleteSymbolicLink(PUNICODE_STRING(&us_string));
+		
+		if (driver_object->DeviceObject != nullptr)
+			IoDeleteDevice(driver_object->DeviceObject);
+
 		// Uninstall vmx on all processors
-		if (!vm_off) {
-			devirtualize_all_processors();
-			vm_off = true;
-		}
+		//if (!vm_off) {
+		//	devirtualize_all_processors();
+		//	vm_off = true;
+		//}
 
 		return;
 	}
@@ -29,7 +36,7 @@ extern "C" {
 	bool vm_off;
 	unsigned g_num_processors;
 
-	auto DriverEntry(_In_ PDRIVER_OBJECT, _In_ PUNICODE_STRING) -> NTSTATUS {
+	auto DriverEntry(_In_ PDRIVER_OBJECT driver_object, _In_ PUNICODE_STRING) -> NTSTATUS {
 		using hv::virtualize_all_processors;
 		using hv::launch_vm;
 		using hv::launch_all_vmms;
@@ -40,6 +47,26 @@ extern "C" {
 		// Opt-in to using non-executable pool memory on Windows 8 and later.
 		// https://msdn.microsoft.com/en-us/library/windows/hardware/hh920402(v=vs.85).aspx
 		ExInitializeDriverRuntime(DrvRtPoolNxOptIn);
+
+		UNICODE_STRING us_drv_string{};
+		RtlInitUnicodeString(PUNICODE_STRING(&us_drv_string), DRV_NAME);
+		PDEVICE_OBJECT dev_obj{};
+
+		auto ntstatus = IoCreateDevice(driver_object, 0, PUNICODE_STRING(&us_drv_string), FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, false, &dev_obj);
+		if (!NT_SUCCESS(ntstatus)) {
+			LOG("[!] Failure creating device object.\n");
+			LOG_ERROR();
+			return STATUS_FAILED_DRIVER_ENTRY;
+		}
+
+		UNICODE_STRING us_dos_string{};
+		RtlInitUnicodeString(PUNICODE_STRING(&us_dos_string), DOS_NAME);
+		ntstatus = IoCreateSymbolicLink(PUNICODE_STRING(&us_dos_string), PUNICODE_STRING(&us_drv_string));
+		if (!NT_SUCCESS(ntstatus)) {
+			LOG("[!] Failure creating dos devices.\n");
+			LOG_ERROR();
+			return STATUS_FAILED_DRIVER_ENTRY;
+		}
 
 		g_num_processors = KeQueryActiveProcessorCount(NULL);
 
