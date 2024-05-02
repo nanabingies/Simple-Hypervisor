fed4dca9b6664e2826523672c75ae96a7e908fa7
Nana Bingies <nanabingies1@gmail.com>
2024-05-02 10:31:04 +0000

    Update Vmx.hpp

--- Simple Hypervisor/Vmx.hpp
+++ Simple Hypervisor/Vmx.hpp
@@ -16,6 +16,7 @@ namespace vmx {
 	auto vmx_check_bios_lock() -> bool;
 	auto vmx_enable_cr4() -> void;
 
+	auto vmx_allocate_vmm_context() -> bool;
 	auto vmx_allocate_vmxon_region(uchar) -> bool;
 	auto vmx_allocate_vmcs_region(uchar) -> bool;
 	auto vmx_allocate_vmexit_stack(uchar) -> bool;
@@ -26,6 +27,8 @@ namespace vmx {
 }
 
 namespace hv {
+	auto vmm_init() -> bool;
+
 	auto virtualize_all_processors() -> bool;
 
 	auto devirtualize_all_processors() -> void;
